e20eb587791727b4b1edfa6d66b965bf73a93e2c
kiransharma13 <kiran1313sharma@gmail.com>
2026-04-30 11:56:11 +0530

    Add support for Vercel deployment in server.js

--- food-ordering/server.js
+++ food-ordering/server.js
@@ -2,15 +2,18 @@ require("dotenv").config();
 const app = require("./app");
 const { ensureDb } = require("./initDb");
 
-const PORT = process.env.PORT || 3000;
-
-ensureDb()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Food ordering app: http://localhost:${PORT}`);
+if (process.env.VERCEL) {
+  module.exports = app;
+} else {
+  const PORT = process.env.PORT || 3000;
+  ensureDb()
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Food ordering app: http://localhost:${PORT}`);
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
     });
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+}
