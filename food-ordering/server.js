65af1460b76788fa3762081284b75fb97e86337e
kiransharma13 <kiran1313sharma@gmail.com>
2026-04-30 14:00:25 +0530

    Refactor server.js to remove VERCEL check

--- food-ordering/server.js
+++ food-ordering/server.js
@@ -2,18 +2,15 @@ require("dotenv").config();
 const app = require("./app");
 const { ensureDb } = require("./initDb");
 
-if (process.env.VERCEL) {
-  module.exports = app;
-} else {
-  const PORT = process.env.PORT || 3000;
-  ensureDb()
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(`Food ordering app: http://localhost:${PORT}`);
-      });
-    })
-    .catch((e) => {
-      console.error(e);
-      process.exit(1);
+const PORT = process.env.PORT || 3000;
+
+ensureDb()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Food ordering app: http://localhost:${PORT}`);
     });
-}
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
